3af5eaa36df78828f9fa86a20c4f9df0ef77e2a6
arnavpatel82 <arnav.patel82@yahoo.com>
2022-05-18 18:35:06 +0800

    added clean_data function

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-import constants
+from constants import PLAYERS
 
+
+def clean_data(player_list):
+    for dic in player_list:
+
+        # cleans up player height
+        dic['height'] = int(dic['height'].replace(' inches', ''))
+
+        # cleans up player experience
+        if dic['experience'] == 'YES':
+            dic['experience'] = True
+        elif dic['experience'] == 'NO':
+            dic['experience'] = False
+        
+
+
+
+        
+    
 
 if __name__ == "__main__":
-    print("hello world")
+    PLAYERS_COPY = PLAYERS.copy()
+    clean_data(PLAYERS_COPY)
